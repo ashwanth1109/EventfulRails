d5518dbe2da97f16af48fd67aeb561e855848830
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-17 14:07:40 +0530

    Basic CRUD set up for user model with just name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+
+    # skip_before_action :verify_authenticity_token
+    # Why didn't the app require this?
+    # Does it have something to do with the --api configuration when creating rails?
+
     def index
-        render json: {message: 'hello rails', status: 200}
+        render json: User.all
     end
+
+    def show
+        render json: User.find(params["id"])
+    end
+
+    def create
+        render json: User.create(params["user"])
+    end
+
+    def delete
+        render json: User.delete(params["id"])
+    end
+
+    def update
+        render json: User.update(params["id"], params["user"])
+    end
+
 end
