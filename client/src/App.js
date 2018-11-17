d2e4970566b6ec7c958ad210fa582f24c3564d90
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-17 22:57:50 +0530

    Added Landing Page Component and Installed Dependency for React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./Custom.css";
+import LandingPage from "./pages/LandingPage";
 
 class App extends Component {
     constructor(props) {
@@ -10,32 +10,21 @@ class App extends Component {
         };
     }
     componentDidMount() {
-        fetch("/users")
-            .then(res => {
-                console.log(res);
-                return res.json();
-            })
-            .then(data => {
-                this.setState({
-                    test: data[0].name
-                });
-            });
+        // fetch("/users")
+        //     .then(res => {
+        //         console.log(res);
+        //         return res.json();
+        //     })
+        //     .then(data => {
+        //         this.setState({
+        //             test: data[0].name
+        //         });
+        //     });
     }
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Hello {this.state.test}</p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div>
+                <LandingPage />
             </div>
         );
     }
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "../Custom.css";
+
+export default class LandingPage extends Component {
+    render() {
+        return <h1>This is the landing page</h1>;
+    }
+}
